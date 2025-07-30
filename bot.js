435fcf53756922739891bd4285ae6be282a903fa
alicejiya5555 <alicejiya124@gmail.com>
2025-07-30 23:38:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const API_KEY = process.env.ETHERSCAN_API;
-const CHECK_INTERVAL = 60 * 1000; // 1 minute polling
+const CHECK_INTERVAL =30 * 60 * 1000 // equals 1,800,000 ms → 30 minutes
 
 const wallets = [
   { name: 'Felix Colombia', address: '0x705A46DdB89F0B6F0c6348b4aF40192778bC0C87' },
